e2ede9572833aa3bf53eabeda231970aeb4b6419
Aarthika <arththika04@gmail.com>
2026-03-19 18:58:37 +0530

    logout

--- Backend/config/passport.js
+++ Backend/config/passport.js
@@ -8,22 +8,34 @@ dotenv.config();
 passport.use(
   new GoogleStrategy(
     {
-      clientID: process.env.GOOGLE_CLIENT_ID,        // MUST NOT be undefined
+      clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "http://localhost:5000/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
+        const email = profile.emails?.[0]?.value;
+
         let user = await User.findOne({ googleId: profile.id });
 
-        if (!user) {
-          user = await User.create({
-            username: profile.displayName,
-            email: profile.emails?.[0]?.value,
-            googleId: profile.id,
-            isVerified: true,
-          });
+        if (!user && email) {
+          user = await User.findOne({ email });
         }
+
+        if (user) {
+          if (!user.googleId) {
+            user.googleId = profile.id;
+            await user.save();
+          }
+          return done(null, user);
+        }
+
+        user = await User.create({
+          username: profile.displayName,
+          email,
+          googleId: profile.id,
+          isVerified: true,
+        });
 
         return done(null, user);
       } catch (err) {
@@ -32,5 +44,18 @@ passport.use(
     }
   )
 );
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
 
 export default passport;
